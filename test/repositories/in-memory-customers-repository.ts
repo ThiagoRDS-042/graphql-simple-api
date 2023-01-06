1e59515041be5d233d2d5fed68bff3553e23b97c
Thiago Rodrigues <thiagords042@gmail.com>
2023-01-05 21:33:40 -0300

    refactor: 💡 adjust test repo

--- test/repositories/in-memory-customers-repository.ts
+++ test/repositories/in-memory-customers-repository.ts
@@ -1,3 +1,4 @@
+import { IFindManyOptions } from "@modules/customers/dtos/find-many-options";
 import { Customer } from "@modules/customers/entities/customer-entity";
 import { ICustomersRepository } from "@modules/customers/repositories/customers-repository";
 
@@ -7,12 +8,39 @@ export class InMemoryCustomersRepository implements ICustomersRepository {
   public async create(customer: Customer): Promise<void> {
     this.customers.push(customer);
   }
+
   public async alreadyExists(email: string): Promise<boolean> {
     const customerAlreadyExists = this.customers.some(
       item => item.email === email,
     );
 
     return customerAlreadyExists;
+  }
+
+  public async findMany(options: IFindManyOptions): Promise<Customer[]> {
+    const { emailContains, nameContains } = options;
+
+    let customers = this.customers;
+
+    if (emailContains) {
+      customers = customers.filter(item => item.email === emailContains);
+    }
+
+    if (nameContains) {
+      customers = customers.filter(item => item.name === nameContains);
+    }
+
+    return customers;
+  }
+
+  public async findById(customerId: string): Promise<Customer | null> {
+    const customer = this.customers.find(item => item.id === customerId);
+
+    if (!customer) {
+      return null;
+    }
+
+    return customer;
   }
 
   // async save(customer: Customer): Promise<void> {
